9fd3f6fac8dc02f010d6d3659afa5b284cb5526f
rodrigosantoscosta <rodrigosc1025@gmail.com>
2026-02-05 00:34:24 -0300

    fix: desabilitar auto-configuração de email

--- src/main/java/br/com/minhavenda/minhavenda/infrastructure/event/listener/PedidoEventListener.java
+++ src/main/java/br/com/minhavenda/minhavenda/infrastructure/event/listener/PedidoEventListener.java
@@ -13,7 +13,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-//@Component
+@Component
 @RequiredArgsConstructor
 public class PedidoEventListener {
 
